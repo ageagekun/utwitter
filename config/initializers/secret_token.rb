a5cadd9030628da4ee0afc323c1589c082f60cd9
ageagekun <fkkn923@gmail.com>
2012-08-11 11:28:54 +0900

    create utwitter project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Utwitter::Application.config.secret_token = 'fcce37ab9a98b6facd86ece808f834faf26a11861da512e7a680eec756bd92072889d00056517370eb9bfa21e274bec51c5892f157f3e6814b2697d8e2daf556'
